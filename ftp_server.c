6642babb42539278e4a32f08e187950a62158ab6
ANUSH S KUMAR <145442617+anushsk030@users.noreply.github.com>
2025-03-22 19:10:14 +0530

    Update ftp_server.c

--- ftp_server.c
+++ ftp_server.c
@@ -1,70 +1,92 @@
-/* FTP Server */
-
 #include <stdio.h>
-#include <sys/types.h>
-#include <netinet/in.h>
-#include <string.h>
 #include <stdlib.h>
+#include <string.h>
 #include <unistd.h>
+#include <arpa/inet.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+
+#define MAX 1024  // Buffer size
 
 int main() {
-    FILE *fp;
-    int sd, newsd, ser, n, a, cli, pid, bd, port, clilen;
-    char name[100], fileread[100], fname[100], ch, file[100], rcv[100];
+    int sockfd, clientfd, port, bind_status;
+    char filename[MAX], filedata[MAX];
     struct sockaddr_in servaddr, cliaddr;
+    socklen_t clilen = sizeof(cliaddr);
+    FILE *fp;
 
-    printf("Enter the port address\n");
+    // Get port number
+    printf("Enter the port number: ");
     scanf("%d", &port);
 
-    sd = socket(AF_INET, SOCK_STREAM, 0);
-    if (sd < 0) {
-        printf("Cant create\n");
-    } else {
-        printf("Socket is created\n");
+    // Create socket
+    sockfd = socket(AF_INET, SOCK_STREAM, 0);
+    if (sockfd < 0) {
+        perror("Socket creation failed");
+        exit(1);
     }
+    printf("Socket created successfully.\n");
 
+    // Initialize server address structure
+    memset(&servaddr, 0, sizeof(servaddr));
     servaddr.sin_family = AF_INET;
-    servaddr.sin_addr.s_addr = htonl(INADDR_ANY);
+    servaddr.sin_addr.s_addr = INADDR_ANY;
     servaddr.sin_port = htons(port);
 
-    a = sizeof(servaddr);
-    bd = bind(sd, (struct sockaddr *)&servaddr, a);
-    if (bd < 0) {
-        printf("Cant bind\n");
-    } else {
-        printf("Binded\n");
+    // Bind the socket
+    bind_status = bind(sockfd, (struct sockaddr *)&servaddr, sizeof(servaddr));
+    if (bind_status < 0) {
+        perror("Bind failed");
+        exit(1);
     }
+    printf("Bind successful.\n");
 
-    listen(sd, 5);
-    clilen = sizeof(cliaddr);
-    newsd = accept(sd, (struct sockaddr *)&cliaddr, &clilen);
+    // Listen for incoming connections
+    listen(sockfd, 5);
+    printf("Waiting for a connection...\n");
 
-    if (newsd < 0) {
-        printf("Cant accept\n");
-    } else {
-        printf("Accepted\n");
+    // Accept client connection
+    clientfd = accept(sockfd, (struct sockaddr *)&cliaddr, &clilen);
+    if (clientfd < 0) {
+        perror("Accept failed");
+        exit(1);
     }
+    printf("Client connected.\n");
 
-    n = recv(newsd, rcv, 100, 0);
-    rcv[n] = '\0';
+    // Receive filename from client
+    int n = recv(clientfd, filename, sizeof(filename) - 1, 0);
+    if (n < 0) {
+        perror("Filename reception failed");
+        close(clientfd);
+        exit(1);
+    }
+    filename[n] = '\0';  // Null-terminate received filename
 
-    fp = fopen(rcv, "r");
+    // Open requested file
+    fp = fopen(filename, "r");
     if (fp == NULL) {
-        send(newsd, "error", 5, 0);
-        close(newsd);
-    } else {
-        while (fgets(fileread, sizeof(fileread), fp)) {
-            if (send(newsd, fileread, sizeof(fileread), 0) < 0) {
-                printf("Can’t send file contents\n");
-            }
-            sleep(1);
-        }
-
-        if (!fgets(fileread, sizeof(fileread), fp)) {
-            send(newsd, "completed", 10, 0);
-        }
+        send(clientfd, "error", strlen("error"), 0);
+        perror("File not found");
+        close(clientfd);
+        exit(1);
     }
 
+    // Send file contents to client
+    while (fgets(filedata, sizeof(filedata), fp)) {
+        send(clientfd, filedata, strlen(filedata), 0);
+        usleep(100000);  // Sleep to avoid flooding client
+    }
+
+    // Send completion message
+    send(clientfd, "completed", strlen("completed"), 0);
+
+    // Close file and socket
+    fclose(fp);
+    close(clientfd);
+    close(sockfd);
+
+    printf("File transfer completed. Server exiting.\n");
     return 0;
 }
+
 
